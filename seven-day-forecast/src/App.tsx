760b441324d27a78a7e3bab333af27f5ba56dad3
Zachary Stehura <zstehura@kent.edu>
2024-07-18 09:37:40 -0400

    add API Callouts

--- seven-day-forecast/src/App.tsx
+++ seven-day-forecast/src/App.tsx
@@ -1,6 +1,162 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+//#region API Callouts
+
+class Forecast {
+  public id: number = 0;
+  public name: string = '';
+	public shortDesc: string = '';
+  public longDesc: string = '';
+  public probOfPrecip: number = 0.0;
+  public temperature: number = 0.0;
+}
+
+class WfoLocation {
+  public gridID: string = '';
+  public gridX: number = 0;
+  public gridY: number = 0;
+  public toURLparams(): string {
+    return `${this.gridID}/${this.gridX},${this.gridY}`
+  }
+}
+
+type coordinates = { x: number, y: number; }
+type GeocodeLocation = { coords: coordinates; }
+type wfoPointResponse = { properties: WfoLocation; }
+type forecastProbOfPrec = { value: number; }
+type halfDayForecast = { number: number; name: string; probabilityOfPrecipitation: forecastProbOfPrec; 
+  shortForecast: string; detailedForecast: string; temperature: number; }
+type forecastProperties = { periods: halfDayForecast[]; }
+type forecastResponse = { properties: forecastProperties; }
+function isCoordinates(o: any): o is coordinates {
+  return 'x' in o && 'y' in o;
+}
+function isGeocodeLocation(o: any): o is GeocodeLocation {
+  return 'coordinates' in o && isCoordinates(o.coordinates);
+}
+function isWfoPointResponse(o: any) : o is wfoPointResponse {
+  return 'properties' in o && isWfoLocation(o.properties);
+}
+function isWfoLocation(o: any): o is WfoLocation {
+  return 'gridID' in o && 'gridX' in o && 'gridY' in o;
+}
+function isForecastProbOfPrec(o:any): o is forecastProbOfPrec {
+  return 'value' in o;
+}
+function isHalfDayForecast(o: any): o is halfDayForecast {
+  return 'number' in o && 'name' in o && 'shortForecast' in o && 'probabilityOfPrecipitation' in o && isForecastProbOfPrec(o.probabilityOfPrecipitation) &&
+    'detailedForecast' in o && 'temperature' in o;
+}
+function isForecastProperties(o: any): o is forecastProperties {
+  return 'periods' in o && isHalfDayForecast(o.periods[0]);
+}
+function isForecastResponse(o: any): o is forecastResponse {
+  return 'properties' in o && isForecastProperties(o.properties);
+}
+
+
+/**
+ * @summary Gets Geocoding data from census.gov and returns a set of coordinates 
+ *
+ * @param {string} streetAddress 
+ * @param {string} city 
+ * @param {string} state
+ * @param {string} zip
+ * @return {*} 
+ */
+async function getLocationCoordinates(streetAddress: string, city: string, state: string, zip: string) {
+  var requestString: string = `https://geocoding.geo.census.gov/geocoder/locations/address?street=${streetAddress}`;
+  if(city !== '') {
+    requestString += `&city=${city}&state=${state}`;
+  }
+  if(zip !== '') {
+    requestString += `&zip=${zip}`;
+  }
+  requestString += '&benchmark=Public_AR_Current&format=json';
+
+  try {
+    const response = await axios.get(requestString);
+    const resObj = JSON.parse(response.data);
+    if(isGeocodeLocation(resObj)) {
+      return `${resObj.coords.y}%2C${resObj.coords.x}`
+    }
+    else {
+      console.log('Error in geocoding response: response=' + resObj);
+    }
+  } catch (error: any) {
+    console.log('Error fetching Location coordinates: ' + error.message);
+  }
+
+  return '0%2C0';
+}
+
+/**
+ * Makes a callout to weather.gov to find information about a specific location so that we can get 
+ * weather forecast there later
+ *
+ * @param {string} coordinates 
+ * @return {WfoLocation} 
+ */
+async function getWfoLocation(coordinates: string) {
+  var requestString: string = `https://api.weather.gov/points/${coordinates}`;
+  var loc: WfoLocation = new WfoLocation();
+  try {
+    const response = await axios.get(requestString);
+    const resObj = JSON.parse(response.data);
+    if(isWfoPointResponse(resObj)) {
+      loc.gridX = resObj.properties.gridX;
+      loc.gridY = resObj.properties.gridY;
+      loc.gridID = resObj.properties.gridID;
+    }
+    else {
+      console.log('Error in WfoLocation response: response=' + resObj);
+    }
+  } catch (error: any) {
+    console.log('Error fetching WfoLocation data: ' + error.message);
+  }
+  return loc;
+}
+
+/**
+ *  Gets a 7 day forecast from weather.gov split into half day intervals (total of 14 array elements)
+ *
+ * @param {WfoLocation} location
+ * @return {Forecast[14]} 
+ */
+async function getForecast(location: WfoLocation) {
+  var requestString: string = `https://api.weather.gov/gridpoints/${location.toURLparams()}/forecast`;
+  var f: Forecast[] = new Array<Forecast>();
+  try {
+    const response = await axios.get(requestString);
+    const resObj = JSON.parse(response.data);
+    if(isForecastResponse(resObj)) {
+      for(var i: number = 0; i++; i < 14) {
+        var temp: Forecast = new Forecast();
+        temp.id = i;
+        temp.longDesc = resObj.properties.periods[i].detailedForecast;
+        temp.name = resObj.properties.periods[i].name;
+        temp.probOfPrecip = resObj.properties.periods[i].probabilityOfPrecipitation.value;
+        temp.shortDesc = resObj.properties.periods[i].shortForecast;
+        temp.temperature = resObj.properties.periods[i].temperature;
+        f.push(temp);
+      }
+    }
+    else {
+      console.log('Error in Forecast response: response=' + resObj);
+    }
+  } catch (error: any) {
+    console.log('Error fetching Forecast data: ' + error.message);
+  }
+
+  return f;
+}
+
+//#endregion API Callouts
+
+
+
 
 function App() {
   return (
